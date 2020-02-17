178e141f4f384546bb5cc00d6c17d29648884b9f
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-02-17 23:54:09 +0200

    Added dynamic BookShelf Component

---
+++ src/BookShelf.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+
+  render() {
+    const { books } = this.props
+
+    return (
+      <ol className="books-grid">
+        {books.map((book) => (
+          <Book book={ book } books={ books } key={ book.id }/>
+        ))}
+      </ol>
+    )
+  }
+
+}
+
+export default BookShelf
