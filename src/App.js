b7b9655bfac73aa026c5bd1b43f0128fc6fb736f
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-02-17 23:00:07 +0200

    Exported BooksList to App file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,84 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import SearchPage from './SearchPage'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import BooksList from './BooksList'
 
 class BooksApp extends React.Component {
-  state = {
-    books : []
-  }
-  componentDidMount() {
-    BooksAPI.getAll()
-        .then((books) => {
-          this.setState(() => ({
-            books
-          }))
-        });
-  }
-  
-  render() {
-    return (
-      <div className="app">
-        <Route path='/search' component={SearchPage} />
-        <Route exact path='/' render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <BooksList books={this.state.books}/>
-            <div className="open-search">
+    state = {
+        books: []
+    }
 
-              <Link
-                  to='/Search'
-               ><button></button></Link>
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
+                this.setState(() => ({
+                    books
+                }))
+            });
+    }
 
+    render() {
+        const books = this.state.books
+        let currentlyReadingBooks = books.filter((b) => {
+            return b.shelf === "currentlyReading"
+        })
+        let wantToReadBooks = books.filter((b) => {
+            return b.shelf === "wantToRead"
+        })
+        let readBooks = books.filter((b) => {
+            return b.shelf === "read"
+        })
+        console.log("App", currentlyReadingBooks)
+        console.log("App", wantToReadBooks)
+        console.log("App", readBooks)
+        return (
+            <div className="app">
+                <Route path='/search' component={SearchPage}/>
+                <Route exact path='/' render={() => (
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>MyReads</h1>
+                        </div>
+                        <div className="list-books-content">
+                            <div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Currently Reading</h2>
+                                    <div className="bookshelf-books">
+                                        <BooksList books={currentlyReadingBooks}/>
+                                    </div>
+                                </div>
+
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Want to Read</h2>
+                                    <div className="bookshelf-books">
+                                        <BooksList books={wantToReadBooks}/>
+                                    </div>
+                                </div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Read</h2>
+                                    <div className="bookshelf-books">
+                                        <BooksList books={readBooks}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="open-search">
+
+                            <Link
+                                to='/Search'
+                            >
+                                <button></button>
+                            </Link>
+
+                        </div>
+                    </div>
+                )}/>
             </div>
-          </div>
-          )} />
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default BooksApp
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,59 +1,25 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BooksList extends Component{
+class BooksList extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired,
-      }
-    render(){
-        const books = this.props
-        // console.log(books)
-        const currentlyReadingBooks = books.books.filter((b) => {
-            return  b.shelf === "currentlyReading"
-            })
-        const wantToReadBooks = books.books.filter((b) => {
-            return  b.shelf === "wantToRead"
-            })
-        const readBooks = books.books.filter((b) => {
-            return  b.shelf === "read"
-            })
-        return(
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {currentlyReadingBooks.map((book)=>(
-                       <Book book={book} />
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {wantToReadBooks.map((book)=>(
-                       <Book book={book} />
+    }
+
+    render() {
+        const {books} = this.props
+        console.log("BookList", books)
+        return (
+            <div>
+                <ol className="books-grid">
+                    {books.map((book) => (
+                        <Book book={book} key={book.id}/>
                     ))}
-                  </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {readBooks.map((book) => (
-                       <Book book={book} />
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-              </div>
+                </ol>
             </div>
         )
     }
 }
+
 export default BooksList
